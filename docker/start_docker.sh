57a44c5a7e60fc545723d09053a8b7c6c619469f
olive004 <olivia.gallup@gmail.com>
2024-02-19 07:45:30 +0000

    modifying mrna all script

---
+++ docker/start_docker.sh
@@ -0,0 +1,21 @@
+
+# A pre-req for using gpus here is the NVIDIA Docker Container Toolkit
+
+sudo docker pull quay.io/biocontainers/intarna:3.3.2--pl5321h7ff8a90_0
+# sudo docker run --rm -it --entrypoint bash quay.io/biocontainers/intarna:3.3.2--pl5321h7ff8a90_0
+sudo docker pull nvidia/cuda:11.8.0-cudnn8-devel-ubuntu22.04
+
+# If image not built yet
+sudo docker build -t genetic_glitch:latest docker
+
+cp ./requirements.txt ./docker
+
+sudo docker create -it \
+--rm \
+--gpus all \
+--name srna \
+--mount type=bind,source="$(pwd)",target=/workdir \
+genetic_glitch:latest
+sudo docker container start srna
+sudo docker exec -it srna /bin/bash 
+# sudo docker container stop srna
