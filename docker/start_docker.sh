8da552d660dc70a73769e87cbe08b7d38f7dadb8
olive004 <olivia.gallup@gmail.com>
2024-10-16 15:08:40 +0100

    new cuda

--- docker/start_docker.sh
+++ docker/start_docker.sh
@@ -3,7 +3,7 @@
 
 sudo docker pull quay.io/biocontainers/intarna:3.3.2--pl5321h7ff8a90_0
 # sudo docker run --rm -it --entrypoint bash quay.io/biocontainers/intarna:3.3.2--pl5321h7ff8a90_0
-sudo docker pull nvidia/cuda:11.8.0-cudnn8-devel-ubuntu22.04
+sudo docker pull nvidia/cuda:12.6.0-cudnn-devel-ubuntu22.04
 
 # If image not built yet
 sudo docker build -t genetic_glitch:latest docker
